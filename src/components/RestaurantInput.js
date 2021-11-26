264b0c937a8adc579f8a6e59d817844444d6e045
yatsun <yatsunlam25@gmail.com>
2021-11-26 01:34:11 -0500

    commit tree 2debe97d6c14d9673e77317612ea9561b145475c

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -24,6 +24,7 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -49,6 +50,22 @@ export class RestaurantInput extends Component {
   }
 };
 
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     addTodo: (todo) => {
+//       dispatch(addTodo(todo))
+//     }
+//   };
+// };
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addRestaurant: (newRestaurant) => {
+      dispatch(addRestaurant(newRestaurant))
+    }
+  }
+}
+
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, mapDispatchToProps)(RestaurantInput)
